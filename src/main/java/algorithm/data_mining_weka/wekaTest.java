4fbfc022d1283bec6241f4256b1e98624feb07bd
Ohmwrecker-123 <2512008088@qq.com>
2022-06-11 17:56:24 +0800

    Weka DataMining Added

---
+++ src/main/java/algorithm/data_mining_weka/wekaTest.java
@@ -0,0 +1,152 @@
+package algorithm.data_mining_weka;
+
+import com.csvreader.CsvReader;
+import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.BayesNet;
+import weka.classifiers.meta.Bagging;
+import weka.classifiers.meta.LogitBoost;
+import weka.classifiers.rules.DecisionTable;
+import weka.classifiers.rules.JRip;
+import weka.classifiers.rules.OneR;
+import weka.classifiers.trees.J48;
+import weka.classifiers.trees.RandomForest;
+import weka.core.Attribute;
+import weka.core.DenseInstance;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.CSVLoader;
+import weka.core.converters.ConverterUtils;
+import weka.gui.beans.DataSource;
+import weka.gui.beans.Loader;
+
+
+import java.io.*;
+import java.nio.Buffer;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * @author Ohmwrecker
+ */
+public class wekaTest {
+    static List<String> attrTags = Arrays.asList("Iris-setosa","Iris-versicolor","Iris-virginica");
+    static List<String> numAttrTags = Arrays.asList("0","1","2","3","4","5","6","7","8","9");
+
+    public static Instances createIrisSample(){
+
+        return new Instances("iris", new ArrayList(Arrays.asList(
+                // 花萼
+                new Attribute("calyxLength"),
+                new Attribute("calyxWidth"),
+                // 花瓣
+                new Attribute("petalLength"),
+                new Attribute("petalWidth"),
+                new Attribute("irisType",attrTags))),0);
+    }
+
+    public static Instances getNumTrainSet() throws Exception {
+
+        CSVLoader csvLoader = new CSVLoader();
+        csvLoader.setSource(new File("C:\\Users\\Ohmwrecker\\Desktop\\作业1\\train.csv"));
+        Instances train = csvLoader.getDataSet();
+        List<Integer> numList = new ArrayList<>();
+        for (int i = 0; i < train.size(); i++) {
+            numList.add(i, (int) train.get(i).value(0));
+        }
+        train.replaceAttributeAt(new Attribute("numType",numAttrTags),0);
+        for (int i = 0; i < train.size(); i++) {
+            train.get(i).setValue(0,numList.get(i));
+        }
+        train.setClassIndex(0);
+        return train;
+    }
+
+    public static Instances getNumTestSet() throws Exception {
+
+        CSVLoader csvLoader = new CSVLoader();
+        csvLoader.setSource(new File("C:\\Users\\Ohmwrecker\\Desktop\\作业1\\test_200.csv"));
+        Instances test = csvLoader.getDataSet();
+        List<Integer> numList = new ArrayList<>();
+        for (int i = 0; i < test.size(); i++) {
+            numList.add(i, (int) test.get(i).value(0));
+        }
+        test.replaceAttributeAt(new Attribute("numType",numAttrTags),0);
+        for (int i = 0; i < test.size(); i++) {
+            test.get(i).setValue(0,numList.get(i));
+        }
+        test.setClassIndex(0);
+        return test;
+    }
+
+    public static ArrayList<String[]> getIrisList() throws IOException {
+        String filePath = "C:\\Users\\Ohmwrecker\\Desktop\\实验2-6.2\\Iris\\iris.csv";
+        ArrayList<String[]> originList = new ArrayList<>();
+
+        //读取csv文件
+        CsvReader reader = new CsvReader(filePath, ',', StandardCharsets.UTF_8);
+        while (reader.readRecord()) {
+            originList.add(reader.getValues());
+        }
+        return originList;
+    }
+
+    public static Instances writeIrisInstances(Instances instances,ArrayList<String[]> originList){
+
+        //创建一个密集向量DenseInstance，写入样本集Instances
+        for (String[] strings : originList) {
+            instances.add(new DenseInstance(1.0, new double[]{
+                    //写入第1列score，数值型
+                    Double.parseDouble(strings[0]),
+                    //写入第2列date，日期型（日期字符串转为Double）
+                    Double.parseDouble(strings[1]),
+                    //写入第3列subject，标称型（标称值转为List下标值）
+                    Double.parseDouble(strings[2]),
+                    //写入第4列name，字符串型（字符串转为Double）
+                    Double.parseDouble(strings[3]),
+                    //第五行
+                    attrTags.indexOf(strings[4]),
+            }));
+            //设置类别属性（标签属性）
+            //设置最后一列为标签
+            instances.setClassIndex(instances.numAttributes() - 1);
+        }
+        return instances;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        Instances data = writeIrisInstances(createIrisSample(), getIrisList());
+        //打乱顺序
+        data.randomize(new Random());
+        //划分训练集和测试集，按8:2的比例
+        int trainSize = (int)(data.size() * 0.8);
+        //将data从下标0开始划一个长度为trainSize的训练集
+        Instances train = new Instances(data, 0, trainSize);
+//        Instances train = getNumTrainSet();
+        //将data从下标trainSize开始划一个长度为data.size() - trainSize的测试集
+        Instances test = new Instances(data, trainSize, data.size() - trainSize);
+//        Instances test = getNumTestSet();
+        //1、创建样本集，划分训练集和测试集
+        //2、模型的创建和训练
+        //创建选项
+        String[] options = new String[1];
+        //不修剪树（不同分类器的选项值参见2.5官方文档）
+        options[0] = "-U";
+        //创建J48决策树分类器对象
+        DecisionTable cls = new DecisionTable();
+        //设置选项
+//        cls.setOptions(options);
+        //训练模型
+        cls.buildClassifier(train);
+        //3、模型的测试（评估）
+        Evaluation eval = new Evaluation(train);
+        //执行测试
+        eval.evaluateModel(cls, test);
+        //打印测试结果摘要信息
+        System.out.println(eval.toSummaryString("Test Summary\n", false));
+
+    }
+}
